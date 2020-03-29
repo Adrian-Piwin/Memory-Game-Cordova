2d9466e54cec991dd952be5706920bd93bd27e63
Adrian <adrian7048@gmail.com>
2020-03-29 15:07:58 -0400

    Added dragging functionality
    Can now drag and drop cards

--- GamePage.js
+++ GamePage.js
@@ -1,39 +1,46 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, PanResponder, Animated } from 'react-native';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import CardList from './CardList'
 import Card from './components/Card'
 import { ScreenOrientation } from 'expo';
+
+
 export default class GamePage extends Component {
 
-    constructor() {
-        super();
-
-        this.state = {
-            dataSource: null
-        }
+    pan = new Animated.ValueXY();
+    panResponder = PanResponder.create({
+    onMoveShouldSetPanResponder: () => true,
+    onPanResponderGrant: () => {
+        this.pan.setOffset({
+        x: this.pan.x._value,
+        y: this.pan.y._value
+        });
+    },
+    onPanResponderMove: Animated.event([
+        null,
+        { dx: this.pan.x, dy: this.pan.y }
+    ]),
+    onPanResponderRelease: () => {
+        this.pan.flattenOffset();
     }
-
-
-
-
-    componentDidMount() {
-
-        this.setState({
-            dataSource: CardList
-        })
-    }
-
+    });
 
     render() {
         return (
             <View style={styles.container}>
-                <Card />
+                <Animated.View
+                style={{
+                    transform: [{ translateX: this.pan.x }, { translateY: this.pan.y }]
+                }}
+                {...this.panResponder.panHandlers}
+                >
+                    <Card />
+                </Animated.View>
             </View>
         )
     }
-
 
 }
 
--- components/Card.js
+++ components/Card.js
@@ -4,28 +4,20 @@ import CardList from '../CardList';
 
 
 const Card = proprs => {
-
     return (
-
         <View style={styles.shadow}>
             <Image style={styles.image} source={CardList[0].imageSource} />
         </View>
     )
-
 }
-
 
 const styles = StyleSheet.create({
     image: {
-        flex: 1,
-        width: 200,
-        height: null,
+        width: 70,
         resizeMode: "contain"
     },
 
     shadow: {
-        alignItems: "center",
-        justifyContent: "center",
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
@@ -35,8 +27,5 @@ const styles = StyleSheet.create({
         shadowRadius: 6.68,
     }
 });
-
-
-
 
 export default Card;
